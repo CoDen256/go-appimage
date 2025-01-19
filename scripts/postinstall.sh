6ca56e26889190b2327cde9d90ae0b45bc19c739
Denys Chernyshov <den.blackshov@gmail.com>
2025-01-19 22:52:30 +0100

    remove linking

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -5,5 +5,5 @@ clean=$(echo $name | sed 's/\.AppImage//g' | sed 's/[0-9.%_-]//g' | tr '[:upper:
 target="$HOME/.local/bin"
 if [[ -n "$name" && ! -e "/usr/local/$name" ]]; then
 	( sleep 5 ; echo "mv $1 $target/$name && ln -s $target/$name $target/$clean" >> ~/stuff) &
-    ( sleep 5 ; mv $1 $target && ln -s $target/$name $target/$clean) &
+    #( sleep 5 ; mv $1 $target && ln -s $target/$name $target/$clean) &
 fi
